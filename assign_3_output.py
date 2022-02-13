d12d99b4dd324a3a979d3f46b7d7ad778f87f0e0
21103023 <96726530+21103023@users.noreply.github.com>
2022-02-13 13:29:29 +0530

    Add files via upload

---
+++ assign_3_output.py
@@ -0,0 +1,71 @@
+
+Enter the sentence:saurabhshubham
+The occurance of letter 'u' is: 2
+The occurance of letter 'm' is: 1
+The occurance of letter 'a' is: 3
+The occurance of letter 's' is: 2
+The occurance of letter 'r' is: 1
+The occurance of letter 'h' is: 3
+The occurance of letter 'b' is: 2
+
+Enter the day:29
+Enter the month:2
+Enter the year:2020
+The next date: 01/3/2020
+
+Desired output:[(2, 4), (4, 16), (6, 36), (8, 64), (9, 81)]
+
+Enter the grade points obtained:9
+Your grade is A and Excellent.
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+Enter the name:sp
+Enter the SID:23
+If you want to feed more details type "Y" else "N":y
+Enter the name:vs
+Enter the SID:67
+If you want to feed more details type "Y" else "N":y
+Enter the name:kn
+Enter the SID:54
+If you want to feed more details type "Y" else "N":y
+Enter the name:helik
+Enter the SID:12
+If you want to feed more details type "Y" else "N":y
+Enter the name:gaj
+Enter the SID:90
+If you want to feed more details type "Y" else "N":n
+Students details are as follows: (SID, Student name) 
+[(23, 'Sp'), (67, 'Vs'), (54, 'Kn'), (12, 'Helik'), (90, 'Gaj')]
+
+Sorted dictionary by student name:[(90, 'Gaj'), (12, 'Helik'), (54, 'Kn'), (23, 'Sp'), (67, 'Vs')]
+
+Sorted dictionary by SID: [(12, 'Helik'), (23, 'Sp'), (54, 'Kn'), (67, 'Vs'), (90, 'Gaj')]
+
+Enter the SID whose name you want to know:12
+The name of the student with SID 12 is :Helik
+
+Number of terms in the resultant Fibonacci Series will be:10
+0,1,1,2,3,5,8,13,21,34,
+
+The sum of the resultant fibonacci series:88
+The average of the resultant Fibonacci series:8.80
+
+set1={1, 2, 3, 4, 5}
+set2={8, 2, 4, 6}
+set3={1, 17, 5, 9, 13}
+
+Set of all elements that are in set1 and set2 but not in both: {1, 3, 5, 6, 8}
+
+Set of all elements that are in only one of the three sets set1, set2 and set3 is: {3, 6, 8, 9, 13, 17}
+
+Set of elements that are in exactly two of the sets set1, set2 and set3:{1, 2, 4, 5}
+
+Set of all integers in the range 1 to 10 that are not in set1:{6, 7, 8, 9, 10}
+
+Set of all integers in the range 1 to 10 that are not in set1, set2 and set3:{10, 7}
