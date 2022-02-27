e4ac3d10b36cb9e09161e9f2d98e5f4ed7341243
21103023 <96726530+21103023@users.noreply.github.com>
2022-02-27 18:29:03 +0530

    Add files via upload

---
+++ assign_4.py
@@ -0,0 +1,134 @@
+#Que.1)
+def toh(n, sr, ax, dt):
+    if n>0:
+        toh(n-1, sr, dt, ax)
+        print("Move disk", n, "from source", sr, "to destination", dt)
+        toh(n-1, ax, sr, dt)
+    
+print(toh(3, 'A', 'B', 'C'))        #Here we are moving all the discs from source peg A to destination peg C
+
+#Que.2)
+#By recursion
+def fact(a):
+    if a==0:
+        return 1
+    else:
+        return a*fact(a-1)
+N=int(input("Enter the number of lines of pascal triangle you want:\t"))  
+def pascal(n):
+    if n>=0:
+        print(" "*(n), end="")
+        def line(r):
+           if r>=0:
+               print(fact(N-1-n)//(fact(r)*fact(N-1-n-r)), end=' ')
+               line(r-1)
+           else:
+               print()
+        line(N-1-n)
+        pascal(n-1)
+if N>0 :
+    pascal(N-1)   
+else:
+    print("Invalid input") 
+print("\n")
+
+#By loops
+if N>0:    
+    for i in range(N):
+        print(" "*(N-i-1), end="")
+        if i==0 :
+            print('1', end='')
+        elif i>0 :
+            for j in range(i+1):
+                print(fact(i)//(fact(j)*fact(i-j)), end=' ')
+        print()        
+else:
+    print("Invalid input")
+
+#Que.3)
+I_no=float(input("\nEnter the first number:"))
+II_no=float(input("Enter the second number:"))
+
+if II_no!=0 :
+    #a)
+    print("\nFunction is callable.")
+    qut, rem=divmod(I_no, II_no)
+    print(f"Quotient obtained={qut}\nReminder obtained={rem:.2f}")
+    #b)
+    if (qut, rem)!=(0,0):
+        print("Values are non-zero.\n")
+    else:
+        print("Values are zero.\n")
+    #c)
+    lt=(qut, rem)+(4, 5, 6)
+    print(lt)
+    lt1=[]
+    for i in lt:
+        if i>4 :
+            lt1.append(i)
+    print("\nThe values which are greater than 4 are:", lt1)
+    #d)
+    print("\nConversion to set datatype:", set(lt1))
+    #e)
+    st=frozenset(lt1)
+    print("\nMaking  the set immutable:", st)
+    #f)
+    print("\nThe maximum value from set is:", max(st))
+    print("The hash value for the maximum value:", hash(max(st)))
+else:
+    print("Function is not callable.")
+
+#Que.4)
+class Student:
+    def __init__(self, name, rollno):
+        self.name=name
+        self.rollno=rollno
+        print("\nInstructor is called for assigning.")
+
+    def __del__(self):
+        print("Destructor is called and object deleted.\n")
+
+st1=Student('saurabh', 23)
+del st1
+
+
+#Que.5)
+class Details:
+    def __init__(self, name, salary):
+        self.name=name
+        self.salary=salary
+
+    def show(self):
+        print(f"{self.name}\t{self.salary}")
+        
+# storing details of employees
+emp1=Details('Mehak', 40000)
+emp2=Details('Ashok', 50000)
+emp3=Details('Viren', 60000)
+print("Name\tSalary")
+emp1.show()
+emp2.show()
+emp3.show()
+
+# updating salary
+emp1.salary=70000
+print("\nThe updated table")
+print("Name\tSalary")
+emp1.show()
+emp2.show()
+emp3.show()
+
+#deleting the record of Viren
+del emp3
+print("\nRecord of Viren deleted.")
+
+#que.6)
+george=input('\nThe word spoken by George:\t').lower()
+barbie=input('The word spoken by Barbie:\t').lower()
+chk=list(george.replace(' ', ''))
+chk1=list(barbie.replace(' ', ''))
+if sorted(chk)==sorted(chk1):
+    print("True friendship.")
+else:
+    print("Friendship is fake.")
+
---
+++ assign_4_output.py
@@ -0,0 +1,66 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:\Users\DELL\AppData\Local\Programs\Python\Python310\assign_4.py ==
+Move disk 1 from source A to destination C
+Move disk 2 from source A to destination B
+Move disk 1 from source C to destination B
+Move disk 3 from source A to destination C
+Move disk 1 from source B to destination A
+Move disk 2 from source B to destination C
+Move disk 1 from source A to destination C
+None
+Enter the number of lines of pascal triangle you want:	6
+     1 
+    1 1 
+   1 2 1 
+  1 3 3 1 
+ 1 4 6 4 1 
+1 5 10 10 5 1 
+
+
+     1
+    1 1 
+   1 2 1 
+  1 3 3 1 
+ 1 4 6 4 1 
+1 5 10 10 5 1 
+
+Enter the first number:67
+Enter the second number:46
+
+Function is callable.
+Quotient obtained=1.0
+Reminder obtained=21.00
+Values are non-zero.
+
+(1.0, 21.0, 4, 5, 6)
+
+The values which are greater than 4 are: [21.0, 5, 6]
+
+Conversion to set datatype: {5, 21.0, 6}
+
+Making  the set immutable: frozenset({5, 21.0, 6})
+
+The maximum value from set is: 21.0
+The hash value for the maximum value: 21
+
+Instructor is called for assigning.
+Destructor is called and object deleted.
+
+Name	Salary
+Mehak	40000
+Ashok	50000
+Viren	60000
+
+The updated table
+Name	Salary
+Mehak	70000
+Ashok	50000
+Viren	60000
+
+Record of Viren deleted.
+
+The word spoken by George:	Saurabh
+The word spoken by Barbie:	bharuas
+True friendship.
